1064b11d0b3f18a19b44a0cf48f3a653e4e4bb2e
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-08 15:18:57 +0530

    implement module in ruby

---
+++ crud.rb
@@ -0,0 +1,42 @@
+module Crud
+  require 'bcrypt'
+puts "Module crud activated"
+  users = [
+            { username: "mashrur", password: "password1" },
+            { username: "jack", password: "password2" },
+            { username: "arya", password: "password3" },
+            { username: "jonshow", password: "password4" },
+            { username: "heisenberg", password: "password5" }
+          ]
+  
+  def Crud.create_hash_digest(password)
+    BCrypt::Password.create(password)
+  end
+  
+  def Crud.verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+  
+  def Crud.create_secure_users(list_of_users)
+    list_of_users.each do |user_record| user_record[:password] = create_hash_digest(user_record[:password])
+    end
+    list_of_users
+  end
+
+  create_secure_users(users)  # method call to create password hash
+
+  def Crud.authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username]==username && verify_hash_digest(user_record[:password])==password
+        return user_record
+      end
+    end
+    "credentials were not correct"
+  end
+
+  #p authenticate_user("heisenberg", "password5", users )
+
+end
+
+
+   
