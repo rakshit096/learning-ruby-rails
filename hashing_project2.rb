3a5904c5778622060f5e42b1a68ac5de0fd37c14
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-08 15:11:04 +0530

    authenticate user method

--- hashing_project2.rb
+++ hashing_project2.rb
@@ -1,24 +1,41 @@
-require 'bcrypt'
+module crud
+  require 'bcrypt'
 
-users = [
-          { username: "mashrur", password: "password1" },
-          { username: "jack", password: "password2" },
-          { username: "arya", password: "password3" },
-          { username: "jonshow", password: "password4" },
-          { username: "heisenberg", password: "password5" }
-        ]
- 
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
- 
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
- 
-def create_secure_users(list_of_users)
-  list_of_users.each do |user_record| user_record[:password] = create_hash_digest(user_record[:password])
+  users = [
+            { username: "mashrur", password: "password1" },
+            { username: "jack", password: "password2" },
+            { username: "arya", password: "password3" },
+            { username: "jonshow", password: "password4" },
+            { username: "heisenberg", password: "password5" }
+          ]
+  
+  def create_hash_digest(password)
+    BCrypt::Password.create(password)
   end
-  list_of_users
+  
+  def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+  
+  def create_secure_users(list_of_users)
+    list_of_users.each do |user_record| user_record[:password] = create_hash_digest(user_record[:password])
+    end
+    list_of_users
+  end
+
+  create_secure_users(users)  # method call to create password hash
+
+  def authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username]==username && verify_hash_digest(user_record[:password])==password
+        return user_record
+      end
+    end
+    "credentials were not correct"
+  end
+
+  p authenticate_user("heisenberg", "password5", users )
+
 end
+
 
