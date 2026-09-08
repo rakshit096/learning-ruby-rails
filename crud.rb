de2e6e6c93baf28eb070f853825f35a8f5171f3d
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-08 16:03:44 +0530

    implement include keyword for including module in class

--- crud.rb
+++ crud.rb
@@ -9,23 +9,23 @@ puts "Module crud activated"
             { username: "heisenberg", password: "password5" }
           ]
   
-  def Crud.create_hash_digest(password)
+  def create_hash_digest(password)
     BCrypt::Password.create(password)
   end
   
-  def Crud.verify_hash_digest(password)
+  def verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
   
-  def Crud.create_secure_users(list_of_users)
+  def create_secure_users(list_of_users)
     list_of_users.each do |user_record| user_record[:password] = create_hash_digest(user_record[:password])
     end
     list_of_users
   end
 
-  create_secure_users(users)  # method call to create password hash
+  # create_secure_users(users)  # method call to create password hash
 
-  def Crud.authenticate_user(username, password, list_of_users)
+  def authenticate_user(username, password, list_of_users)
     list_of_users.each do |user_record|
       if user_record[:username]==username && verify_hash_digest(user_record[:password])==password
         return user_record
